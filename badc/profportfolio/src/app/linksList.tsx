1531e8e210009087c1858ae02c7ce3cf871a5ae4
Ben Arno <bdarno92@gmail.com>
2023-09-25 14:15:56 -0700

    restructured assets, added links

--- badc/profportfolio/src/app/linksList.tsx
+++ badc/profportfolio/src/app/linksList.tsx
@@ -1,21 +1,35 @@
-import { link } from "fs";
 import Link from "next/link";
+import linkData from '../../public/assets/json/links.json';
 
 export default function linksList() {
-  
-  let adrses = [
-    {
-      link: 'https://www.linkedin.com/in/ben-arno/',
-      name: 'LinkedIn'
-    },
-    {
-      link: 'https://github.com/Barnord',
-      name: 'GitHub'
-    }];
+
+  let adrses = linkData.adrses
   let linkList: any[] = [];
+
   adrses.forEach(adrs => {
-    linkList.push(<li><a href={adrs.link}>{adrs.name}</a></li>)
-  })
+      linkList.push(
+        <li>
+          <Link href={adrs.link} target='_blank'>
+            {adrs.name}
+          </Link>
+        </li>
+      )
+
+    if (adrs.xtra != null) {
+      let xtraLinkList: any[] = [];
+
+      adrs.xtra.forEach(xtra =>
+        xtraLinkList.push(
+          <li>
+            <Link href={xtra.link} target='_blank'>
+              {xtra.name}
+            </Link>
+          </li>
+        ));
+      
+      linkList.push(<ul>{xtraLinkList}</ul>)
+      }
+    })
 
   return(
     <>
@@ -25,3 +39,4 @@ export default function linksList() {
     </>
   );
 }
+
