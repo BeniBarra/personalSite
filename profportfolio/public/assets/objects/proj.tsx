1c436dcab303d930abe324507441fe902db19cdf
Ben Arno <bdarno92@gmail.com>
2023-09-28 18:02:52 -0700

    everything but the projects carousel itself is working

---
+++ profportfolio/public/assets/objects/proj.tsx
@@ -0,0 +1,13 @@
+type Project = {
+  name: string;
+  imgs: string[];
+  info: string[];
+
+  // constructor(title: string, pics: string[], dscrpt: string[]) {
+  //   this.name = title;
+  //   this.imgs = pics;
+  //   this.info = dscrpt;
+  // }
+};
+
+export default Project;
--- profportfolio/src/app/page.tsx
+++ profportfolio/src/app/page.tsx
@@ -10,12 +10,13 @@ import {
 // initTE({ Carousel });
 
 export default function Home() {
+  // initTE({Carousel})
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-3xl font-bold underline">Lets just slam pow an image in here real quick.</h1>
         {Profile()}
         {linksList()}
-        {carouselItem()}
+        {/* {carouselItem()} */}
         {Projects()}
     </main>
   )
